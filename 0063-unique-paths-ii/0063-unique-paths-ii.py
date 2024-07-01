d835f275576479c3ec44ab0334e60e5c3a412e81
ChiaHan1 <60413842+ChiaHan1@users.noreply.github.com>
2024-07-01 00:21:25 -0700

    Time: 50 ms (20.12%), Space: 16.3 MB (99.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -4,8 +4,11 @@ class Solution:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
         
         dp = [[0 for i in range(n)] for j in range(m)]
+        
+        # if the starting point is an bostacle
         if obstacleGrid[0][0]:
             return 0
+        
         dp[0][0] = 1
         
         for i in range(m):
@@ -13,6 +16,7 @@ class Solution:
                 # ignore the obstacle
                 if obstacleGrid[i][j]:
                     continue
+                # can come from top and left
                 if i - 1 >= 0 and j - 1 >= 0:
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
                 elif i - 1 >= 0:
