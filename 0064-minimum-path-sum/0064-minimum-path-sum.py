20ab5c5287c4626183f3236e829b8ef29bfad630
ChiaHan1 <60413842+ChiaHan1@users.noreply.github.com>
2024-07-02 00:22:21 -0700

    Time: 83 ms (64.99%), Space: 18.1 MB (89.31%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -3,22 +3,24 @@ class Solution:
         # get the dimension of the grid
         m, n = len(grid), len(grid[0])
         
-        # initialize a 2d array with 0s
+        # initialize an m x n array with 0s
         dp = [[0] * n for _ in range(m)]
         
+        # base case - the starting point
+        dp[0][0] = grid[0][0]
+
         for i in range(m):
             for j in range(n):
-                # base case - the starting point
-                if i == 0 and j == 0:
-                    dp[i][j] = grid[i][j]
                 # if (i, j) is not at the top row or leftmost column
-                elif i - 1 >= 0 and j - 1 >= 0:
+                if i - 1 >= 0 and j - 1 >= 0:
                     dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
                 # if (i, j) is at the leftmost column
                 elif i - 1 >= 0:
                     dp[i][j] = dp[i - 1][j] + grid[i][j]
                 # if (i, j) is at the top row
-                else:
+                elif j - 1 >= 0:
                     dp[i][j] = dp[i][j - 1] + grid[i][j]
-                    
+                # skipping base case
+                else:
+                    continue
         return dp[m - 1][n - 1]
