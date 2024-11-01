1d2ab3534abe38dd8ded7a179917a39a9c20c8a2
ChiaHan1 <60413842+ChiaHan1@users.noreply.github.com>
2024-11-02 03:03:08 +0800

    Time: 0 ms (100.00%), Space: 7.8 MB (42.98%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.c
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.c
@@ -1,12 +1,10 @@
 char repeatedCharacter(char* s) {
     uint32_t bitmap = 0;
-    int len = strlen(s);
-    for (int i = 0; i < len; i++) {
-        int key = 1 << (int) (s[i] - 'a');
-        if (bitmap & key) {
+    for (int i = 0; i < strlen(s); i++) {
+        if (bitmap & (1 << (int) (s[i] - 'a'))) {
             return s[i];
         }
-        bitmap = bitmap | key;
+        bitmap = bitmap | (1 << (int) (s[i] - 'a'));
     }
     return s[0];
 }
