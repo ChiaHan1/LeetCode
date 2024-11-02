ed6a801196c0fb9d86a20ddbbd95c7e0a5aaef36
ChiaHan1 <60413842+ChiaHan1@users.noreply.github.com>
2024-11-02 08:40:07 +0800

    Time: 0 ms (100.00%), Space: 9.3 MB (52.51%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.c
+++ 0036-valid-sudoku/0036-valid-sudoku.c
@@ -1,21 +1,24 @@
 bool isValidSudoku(char** board, int boardSize, int* boardColSize) {
     // bitmap for 9 rows, 9 columns, and 9 squares
-    uint16_t bitmap_row[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    uint16_t bitmap_col[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    uint16_t bitmap_sq[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    short bitmap_row[9] = {0};
+    short bitmap_col[9] = {0};
+    short bitmap_sq[9] = {0};
     for (int i = 0; i < 9; ++i) {
         for (int j = 0; j < 9; ++j) {
             if (board[i][j] == '.') continue;
-            // covert the ascii value to an int
-            int num = board[i][j] - '0';
+            // covert the ascii value to an int then subtract by 1 for shifting
+            int shift = board[i][j] - '1';
+            int bit = 1 << shift;
             // check the row
-            if (bitmap_row[i] & (1 << num)) return false;
-            bitmap_row[i] |= (1 << num);
+            if (bitmap_row[i] & bit) return false;
+            bitmap_row[i] |= bit;
             // check the column
-            if (bitmap_col[j] & (1 << num)) return false;
-            bitmap_col[j] |= (1 << num);
-            if (bitmap_sq[(i / 3) * 3 + (j / 3)] & (1 << num)) return false;
-            bitmap_sq[(i / 3) * 3 + (j / 3)] |= (1 << num);
+            if (bitmap_col[j] & bit) return false;
+            bitmap_col[j] |=  bit;
+            // check the square
+            int idx = (i / 3) * 3 + (j / 3);
+            if (bitmap_sq[idx] & bit) return false;
+            bitmap_sq[idx] |=  bit;
         }
     }
     return true;
